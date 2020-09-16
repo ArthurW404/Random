c857b4c581a188ffdacdd7c68b28365a4c74d36a
ArthurW404 <48990553+ArthurW404@users.noreply.github.com>
2020-09-16 23:09:33 +1000

    Delete namespacingVar.pl

--- namespacingVar.pl
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/perl -w
-
-$davidGay = "DavidGay";
-@davidGay = qw/David Gay/;
-%davidGay = ("david" => "gay", 0 => "david Gay nuts");
-print $davidGay, " ", $davidGay[0], " ", $davidGay{"david"}, " ", $davidGay{0},;
